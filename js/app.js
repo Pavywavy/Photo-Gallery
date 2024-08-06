88446c385ce320a4346ce14d16fab453ad1bab5d
Pavlos <pavlosvalamontes@gmail.com>
2024-08-06 14:50:14 -0400

    added premade function to allow search function to work for gallery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-baguetteBox.run('.gallery');
+window.addEventListener('load', function() {
+  baguetteBox.run('.gallery');
+});
 
 //line for search bar
 const search = new Filter('search', 'data-caption');
